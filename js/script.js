dff42f6e01b2c4d5494334c83e1b68afb6e494e2
Maycon Santos De Souza <133885937+mayckinm@users.noreply.github.com>
2023-07-07 16:37:58 -0300

    criada a var acesso entrada

--- js/script.js
+++ js/script.js
@@ -2,3 +2,5 @@
 var categoria = document.getElementById('categoria')
 //retira do html o id acesso//
 var ecesso = document.getElementById('acesso')
+//seleciona o elemento com o seu id no html//
+var acessoEntrada = document.getElementById("acesso-entrada")
